05915b66bee6f8a2d881dfc456688c6da517476f
lei <lei@princeton.edu>
2016-04-29 11:30:12 -0400

    add stuff for adjoint weighting

---
+++ scripts/adjoint_weights/calculate_adjoint_weights.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Calculate the adjoint source weighting based on the station and source
+distribution
+
+:copyright:
+    Wenjie Lei (lei@princeton.edu), 2016
+:license:
+    GNU Lesser General Public License, version 3 (LGPLv3)
+    (http://www.gnu.org/licenses/lgpl-3.0.en.html)
+"""
