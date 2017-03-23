cf3dcb5c4ed5d0c010bf0dc1d492d05188c94a06
lei <lei@princeton.edu>
2017-03-23 13:35:25 -0400

    add pyyaml depandency

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     ],
     install_requires=[
         "numpy", "obspy>=1.0.0", "flake8>=3.0", "pytest", "nose",
-        "future>=0.14.1", "pytomo3d", "pyasdf"
+        "future>=0.14.1", "pytomo3d", "pyasdf", "pyyaml"
     ],
     entry_points={
         'console_scripts': consoles
